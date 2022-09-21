0da4ab421106d21b9965dc5b061280f59d2a258b
= <willowcarter42@gmail.com>
2022-09-21 14:20:27 -0600

    barebones structure

---
+++ dist/assessment/assessment.js
@@ -0,0 +1,3 @@
+//this is where the logic for handling the buckets will go
+//
+//once we start adding in the assessment functionality
---
+++ dist/components/questiondata.js
@@ -0,0 +1,2 @@
+//this is where we can define the format of the data for a
+// question, the correct answer, and the foil answers
