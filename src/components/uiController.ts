b093e53cd87b0e4ec1f4484553be5408e4a73356
= <willowcarter42@gmail.com>
2022-09-22 22:06:07 -0600

    starting to define question display function

--- src/components/uiController.ts
+++ src/components/uiController.ts
@@ -1,7 +1,61 @@
-//functions to show/hide the different containers
+import { qData, answerData } from './questionData';
+
+
 const landingCont = document.getElementById("landWrap");
 const gameCont = document.getElementById("gameWrap");
 const endCont = document.getElementById("endWrap");
+
+const b1 = document.getElementById("answerButton1");
+const b2 = document.getElementById("answerButton2");
+const b3 = document.getElementById("answerButton3");
+const b4 = document.getElementById("answerButton4");
+
+const buttons = [b1, b2, b3, b4];
+
+
+//add button listeners
+
+b1.addEventListener("click", function() {
+	buttonPress(1);
+});
+
+b2.addEventListener("click", function() {
+	buttonPress(2);
+});
+
+b3.addEventListener("click", function() {
+	buttonPress(3);
+});
+
+b4.addEventListener("click", function() {
+	buttonPress(4);
+});
+
+
+//function to display a new question
+export function showQuestion(newQ: qData): void{
+
+
+	//// TODO: show the question prompt
+
+		//showing the answers on each button
+		for (var aNum in newQ.answers){
+			let curAnswer = newQ.answers[aNum];
+			let answerCode = "";
+			if ('answerText' in curAnswer){
+				answerCode += curAnswer.answerText;
+			}
+			else if ('answerImg' in curAnswer){
+				answerCode += "<img src='" + curAnswer.answerImg + "'";
+			}
+			buttons[aNum].innerHTML = answerCode;
+		}
+
+}
+
+
+
+//functions to show/hide the different containers
 
 export function showLanding(): void{
 	landingCont.style.display = "block";
@@ -21,24 +75,7 @@ export function showEnd(): void{
 	endCont.style.display = "block";
 }
 
-//add button listeners
-
-document.getElementById("answerButton1").addEventListener("click", function() {
-	buttonPress(1);
-});
-
-document.getElementById("answerButton2").addEventListener("click", function() {
-	buttonPress(2);
-});
-
-document.getElementById("answerButton3").addEventListener("click", function() {
-	buttonPress(3);
-});
-
-document.getElementById("answerButton4").addEventListener("click", function() {
-	buttonPress(4);
-});
-
+//handle button press
 
 function buttonPress(num: number){
 	console.log(num);
