4ef42ae697329f61c012fc16e8fb9af6da219f69
= <willowcarter42@gmail.com>
2022-09-24 23:03:13 -0600

    changing bundle outfile location, excluding dist

--- dist/assessment/assessment.js
+++
@@ -1,3 +0,0 @@
-//this is where the logic for handling the buckets will go
-//
-//once we start adding in the assessment functionality
--- dist/components/questiondata.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// this is where we can define the format of the data for a
-// question, the correct answer, and the foil answers
-Object.defineProperty(exports, "__esModule", { value: true });
