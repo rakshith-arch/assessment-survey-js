fe2f5625c1da2d40e2f3cb25bb2d038a58d84662
george dzavashvili <dzavashviligeorge@gmail.com>
2023-08-25 10:05:31 +0400

    Just a bit of formatting

--- src/components/uiController.ts
+++ src/components/uiController.ts
@@ -91,12 +91,10 @@ export function readyForNext(newQ: qData): void {
 	pB.innerHTML = "<button id='nextqButton'><svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 18L15 12L9 6V18Z' fill='currentColor' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path></svg></button>";
 	var nqb = document.getElementById("nextqButton");
 	nqb.addEventListener("click", function (){
-
-	showQuestion();
-			playAudio(newQ.promptAudio, showOptions);
-
-
+		showQuestion();
+		
 		//playquestionaudio
+		playAudio(newQ.promptAudio, showOptions);
 	})
 }
 
@@ -125,7 +123,6 @@ export function showQuestion(newQ?: qData): void {
 	}
 
 	if ('promptImg' in newQ) {
-
 		var tmpimg = getImg(newQ.promptImg);
 		qT.appendChild(tmpimg);
 	}
