364f358ac1e0688badfc4c87c0845f9687c2f749
george dzavashvili <dzavashviligeorge@gmail.com>
2023-08-25 10:07:17 +0400

    File downloader checker and converter beginning

---
+++ fdcc.py
@@ -0,0 +1,15 @@
+# File downloader and converter tool to get data from the web and convert it to a format that can be used by the app
+
+config = {
+    driveURL: "",
+    csvURL: "",
+    tempDir: "tempDir",
+    outputDir: "./outputDir",
+    inputAudioExt: ".wav",
+    outputAudioExt: ".mp3",
+    doConversion: true,
+    doAudioAndCSVIntergirtyCheck: true,
+}
+
+
+
