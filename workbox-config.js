957f64ff79dcae7640f7a9d32d98ad2f8bdda126
george dzavashvili <dzavashviligeorge@gmail.com>
2023-08-25 10:06:45 +0400

    Add workbox config for manifest injection

---
+++ workbox-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    globDirectory: "./",
+    globPatterns: ["**/*.{wav,mp3,WAV,png,webp,otf,jpg,js,json,css,html}"],
+    swDest: "sw.js",
+    swSrc: "sw-src.js",
+    globIgnores:[
+        "audio/**/*", 
+        "node_modules/**/*",
+        "data/**/*",
+        "src/**/*",
+        "workbox-config.js",
+        "sw-src.js",
+        "tsconfig.json",
+        "webpack.config.js",
+        "package.json",
+        "package-lock.json",
+        "README.md",
+        "workbox-7f917042.js",
+        "workbox-7f917042.js.map",
+        "stats.json"
+    ],
+};
