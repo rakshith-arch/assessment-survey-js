345112ccf7bc6f35be72afd56dbbc2c8cfd3ffa6
george dzavashvili <dzavashviligeorge@gmail.com>
2023-09-01 11:58:53 +0400

    Version bump

--- src/App.ts
+++ src/App.ts
@@ -13,7 +13,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from 'firebase/analytics';
 import { Workbox } from 'workbox-window';
 
-const appVersion = "v0.0.1";
+const appVersion = "v0.0.2";
 
 
 export class App {
