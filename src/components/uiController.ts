2caff67e23f0997162fbe28d5463e3d8945e9acc
george dzavashvili <dzavashviligeorge@gmail.com>
2023-09-12 19:40:40 +0400

    Add the new svg instead of the old one for next button

--- src/components/uiController.ts
+++ src/components/uiController.ts
@@ -91,7 +91,8 @@ export function readyForNext(newQ: qData): void {
 	nextquest = newQ;
 	qT.innerHTML = "";
 	qT.style.display = "none";
-	pB.innerHTML = "<button id='nextqButton'><svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 18L15 12L9 6V18Z' fill='currentColor' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path></svg></button>";
+	// pB.innerHTML = "<button id='nextqButton'><svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 18L15 12L9 6V18Z' fill='currentColor' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'></path></svg></button>";
+	pB.innerHTML = "<button id='nextqButton'><img width='70px' height='70px' src='/img/sound-play-button.svg' type='image/svg+xml'> </img></button>";
 	var nqb = document.getElementById("nextqButton");
 	nqb.addEventListener("click", function () {
 		showQuestion();
