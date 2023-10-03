a684afde2439d7e98f9297f7ad76ef25f8bed999
george dzavashvili <dzavashviligeorge@gmail.com>
2023-10-03 19:05:17 +0400

    Add cache model class that implements ICacheModel

--- src/components/cacheModel.ts
+++ src/components/cacheModel.ts
@@ -6,3 +6,17 @@ interface ICacheModel {
     contentFilePath: string;
     audioVisualResources: string[];
 }
+
+class CacheModel implements ICacheModel {
+    appName: string;
+    contentFilePath: string;
+    audioVisualResources: string[];
+
+    constructor(appName: string, contentFilePath: string, audioVisualResources: string[]) {
+        this.appName = appName;
+        this.contentFilePath = contentFilePath;
+        this.audioVisualResources = audioVisualResources;
+    }
+}
+
+export default CacheModel;
